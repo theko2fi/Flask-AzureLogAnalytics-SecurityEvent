158495ad8e035fb9ce2e6fc3bb5a0475f60938a4
theko2fi <koffikenneth7@gmail.com>
2022-07-20 00:21:51 +0100

    updated query

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def hello_world():
 @app.route("/data")
 def fetch_data():
 
-    query ="SecurityEvent | where EventID == 4634 or EventID == 4800 or EventID == 4624 | where Account contains 'alex' | project localTimeGenerated = TimeGenerated + 1h,Activity,Account | summarize by format_datetime(localTimeGenerated, 'dd/MM/yyyy'),format_datetime(localTimeGenerated, 'HH:mm'),Activity,Account"
-    #query = "SecurityEvent | where EventID == 4634 or EventID == 4800 or EventID == 4624 | where Account contains 'alex'  | project localTimeGenerated=TimeGenerated+1h,Activity,Account,LogonType | summarize by localTimeGenerated,Activity,Account"
+    query = "SecurityEvent | where TargetAccount contains "azure" and ((EventID==4624 and (LogonType ==2 or LogonType == 11)) or (EventID == 4800) or (EventID == 4801) or (EventID == 4647) or (EventID == 4802) or (EventID == 4803) or (EventID == 4608))| project TimeGenerated,LogonTypeName,Activity,TargetUserName,TargetLogonId | summarize take_any(Activity,TargetUserName,TargetLogonId) by letemps=format_datetime(TimeGenerated+1h, 'dd/MM/yyyy HH:mm') | sort by letemps desc"
     sp_token = LogAnalyticsDataReader.get_token(tenant=app.config["TENANT"], sp_id=app.config["SP_ID"], sp_secret=app.config["SP_SECRET"])
     data = LogAnalyticsDataReader.get_data(query=query,token=sp_token, azure_log_customer_id=app.config["AZURE_LOG_CUSTOMER_ID"])
     return data
